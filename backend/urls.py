1bca86543bac05fdecb13cf6cdd2485632db6fd4
danielkjellid <daniel.kjellid@gmail.com>
2020-06-16 21:33:58 +0200

    feature: started working on user registration/login

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from django_registration.backends.activation.views import RegistrationView
+from users.forms import UserRegistrationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bruker/registrer/', RegistrationView.as_view(form_class=UserRegistrationForm, success_url='/'), name='django_registration-register'),
+    path('bruker/', include('django_registration.backends.activation.urls')),
+    path('bruker/', include('django.contrib.auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
