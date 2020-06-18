22c3846f1a8791b70c6538bea2c7f48329a64491
danielkjellid <daniel.kjellid@gmail.com>
2020-06-18 13:58:27 +0200

    feature: added frontend

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib.auth import views as auth_views
-
 from django_registration.backends.one_step.views import RegistrationView
+
+from core.views import IndexTemplateView
 from users.forms import UserAuthenticationForm, UserRegistrationForm
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('bruker/logg-inn/', auth_views.LoginView.as_view(authentication_form=UserAuthenticationForm)),
     path('bruker/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    re_path(r'^.*$', IndexTemplateView.as_view(), name='entry-point')
 ]
