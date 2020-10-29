90ccae58a7670f2257d75bec0f66096c100ca13e
danielkjellid <daniel.kjellid@gmail.com>
2020-10-29 15:42:51 +0100

    feature: added admin instances for kitchen variants

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 from inventory.models.category import Category, SubCategory
-from inventory.models.kitchen import Kitchen, KitchenExample
-from inventory.models.product import (Product, ProductFile, ProductImage,
-                                      ProductVariant)
 from inventory.models.supplier import Supplier
+from inventory.models.product import Product, ProductVariant, ProductFile, ProductImage
+from inventory.models.kitchen import Kitchen, KitchenExample, KitchenSilkColor, KitchenDecor, KitchenPlywood, KitchenLaminateColor, KitchenExclusiveColor, KitchenTrendColor
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -68,3 +67,29 @@ admin.site.register(Supplier, SupplierAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductVariant, ProductVariantAdmin)
 admin.site.register(Kitchen, KitchenAdmin)
+
+# to be removed
+class KitchenSilkAdmin(admin.ModelAdmin):
+    model = KitchenSilkColor
+
+class KitchenDecorAdmin(admin.ModelAdmin):
+    model = KitchenDecor
+
+class KitchenPlywoodAdmin(admin.ModelAdmin):
+    model = KitchenPlywood
+
+class KitchenLaminateAdmin(admin.ModelAdmin):
+    model = KitchenLaminateColor
+
+class KitchenExclusiveAdmin(admin.ModelAdmin):
+    model = KitchenExclusiveColor
+
+class KitchenTrendAdmin(admin.ModelAdmin):
+    model = KitchenTrendColor
+
+admin.site.register(KitchenSilkColor, KitchenSilkAdmin)
+admin.site.register(KitchenDecor, KitchenDecorAdmin)
+admin.site.register(KitchenPlywood, KitchenPlywoodAdmin)
+admin.site.register(KitchenLaminateColor, KitchenLaminateAdmin)
+admin.site.register(KitchenExclusiveColor, KitchenExclusiveAdmin)
+admin.site.register(KitchenTrendColor, KitchenTrendAdmin)
