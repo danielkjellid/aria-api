d57382d764556d453da8baf04f465a5f8dce83ed
danielkjellid <daniel.kjellid@gmail.com>
2021-11-19 21:23:51 +0100

    Serializer optimization

--- core/serializers.py
+++ core/serializers.py
@@ -15,14 +15,18 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 
 class BaseHeaderImageSerializer(serializers.Serializer):
+    """
+    A serializer that serializes header images. Values
+    are inherited by BaseHeaderImageModel in core.models.
+    """
 
-    apply_filter = serializers.BooleanField()
-    image_512x512 = serializers.SerializerMethodField()
-    image_640x275 = serializers.SerializerMethodField()
-    image_1024x1024 = serializers.SerializerMethodField()
-    image_1024x575 = serializers.SerializerMethodField()
-    image_1536x860 = serializers.SerializerMethodField()
-    image_2048x1150 = serializers.SerializerMethodField()
+    apply_filter = serializers.ImageField()
+    image_512x512 = serializers.ImageField()
+    image_640x275 = serializers.ImageField()
+    image_1024x1024 = serializers.ImageField()
+    image_1024x575 = serializers.ImageField()
+    image_1536x860 = serializers.ImageField()
+    image_2048x1150 = serializers.ImageField()
 
     class Meta:
         fields = (
@@ -35,24 +39,3 @@ class BaseHeaderImageSerializer(serializers.Serializer):
             'image_2048x1150'
         )
         abstract = True
-
-    def _get_image(self, image):
-        return os.path.join(settings.MEDIA_URL, str(image))
-
-    def get_image_512x512(self, image):
-        return self._get_image(image.image_512x512)
-
-    def get_image_640x275(self, image):
-        return self._get_image(image.image_640x275)
-
-    def get_image_1024x1024(self, image):
-        return self._get_image(image.image_1024x1024)
-
-    def get_image_1024x575(self, image):
-        return self._get_image(image.image_1024x575)
-
-    def get_image_1536x860(self, image):
-        return self._get_image(image.image_1536x860)
-
-    def get_image_2048x1150(self, image):
-        return self._get_image(image.image_2048x1150)
