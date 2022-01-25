88563d2b9e926df13d497ed6eca792060cfa2398
danielkjellid <daniel.kjellid@gmail.com>
2022-01-25 14:33:18 +0100

    Remove translations on admin fields, and update translations.

--- aria/users/models.py
+++ aria/users/models.py
@@ -29,88 +29,75 @@ class User(AbstractBaseUser, PermissionsMixin):
     """
 
     email = models.EmailField(
-        _("email address"),
         unique=True,
     )
     first_name = models.CharField(
-        _("first name"),
         max_length=255,
         unique=False,
     )
     last_name = models.CharField(
-        _("last name"),
         max_length=255,
         unique=False,
     )
-    birth_date = models.DateField(_("birth date"), null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
     avatar_color = models.CharField(
-        _("avatar color"),
         max_length=8,
         unique=False,
         choices=AvatarColors.choices,
     )
     phone_number = models.CharField(max_length=30)
     has_confirmed_email = models.BooleanField(default=False)
-    street_address = models.CharField(_("street name"), max_length=255, unique=False)
+    street_address = models.CharField(max_length=255, unique=False)
     zip_code = models.CharField(
-        _("zip code"),
         max_length=20,
         unique=False,
     )
     zip_place = models.CharField(
-        _("postal place"),
         max_length=255,
         unique=False,
     )
     disabled_emails = models.BooleanField(
-        _("disabled email"),
         default=False,
-        help_text=_(
+        help_text=(
             "Decides if a user receives email from us. "
             "Typically used if we do not want a user to receive marketing (competetors)."
         ),
     )
     subscribed_to_newsletter = models.BooleanField(
-        _("subscribed to newsletter"),
         default=True,
-        help_text=_("Decides if a user receives marketing emails from us."),
+        help_text=("Decides if a user receives marketing emails from us."),
     )
     allow_personalization = models.BooleanField(
-        _("allows personalization"),
         default=True,
-        help_text=_(
+        help_text=(
             "Decides if a user accepts a personalized experience within the app."
         ),
     )
     allow_third_party_personalization = models.BooleanField(
-        _("allows third party personalization"),
         default=True,
-        help_text=_(
+        help_text=(
             "Decides if we share user cookies with external sources such as Facebook. "
             "This will make the user see adverts and other related content to the app."
         ),
     )
     acquisition_source = models.CharField(
-        _("acquisition source"), blank=True, max_length=255, unique=False, null=True
+        blank=True, max_length=255, unique=False, null=True
     )
-    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    date_joined = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(
-        _("active"),
         default=True,
-        help_text=_(
+        help_text=(
             "Designates whether this user should be treated as active. "
             "Unselect this instead of deleting accounts if you want to preserve the data."
         ),
     )
     is_staff = models.BooleanField(
-        _("staff status"),
         default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
+        help_text=("Designates whether the user can log into this admin site."),
     )
     is_superuser = models.BooleanField(
-        _("superuser status"),
         default=False,
-        help_text=_(
+        help_text=(
             "Designates whether the user is automatically granted all permissions."
         ),
     )
@@ -121,8 +108,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
+        verbose_name = "user"
+        verbose_name_plural = "users"
         permissions = (
             ("has_users_list", "Can list users"),
             ("has_user_edit", "Can edit a single user instance"),
