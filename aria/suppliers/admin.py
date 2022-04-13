b0f23a840e4c579abbaeeecf75b03bd2f881ecdd
danielkjellid <daniel.kjellid@oda.com>
2022-04-13 12:24:06 +0200

    Remove suppliers from PR

--- aria/suppliers/admin.py
+++ aria/suppliers/admin.py
@@ -6,6 +6,7 @@ from aria.suppliers.models import Supplier
 class SupplierAdmin(admin.ModelAdmin):
     model = Supplier
     list_display = ("name", "contact_email", "origin_country")
+    filter_horizontal = ("sites",)
     list_filter = ["is_active"]
     ordering = ["name"]
 
