c843041492095c6a482f5f6e2c3113ff9883c7b3
danielkjellid <daniel.kjellid@oda.com>
2022-04-13 12:52:02 +0200

    Add auth from v2

---
+++ aria/auth/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from aria.auth.viewsets import (
+    AuthLogoutAndBlacklistRefreshTokenForUserAPI,
+    AuthTokenObtainAPI,
+)
+
+internal_patterns = []
+
+public_patterns = [
+    path("tokens/obtain/", AuthTokenObtainAPI.as_view(), name="auth-tokens-obtain"),
+    path("tokens/refresh/", TokenRefreshView.as_view(), name="auth-tokens-refresh"),
+    path(
+        "tokens/blacklist/",
+        AuthLogoutAndBlacklistRefreshTokenForUserAPI.as_view(),
+        name="auth-tokens-blacklist",
+    ),
+]
+
+urlpatterns = internal_patterns + public_patterns
