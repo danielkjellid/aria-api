0c16730ee88ba9166c1bed4be3408635a98f6aba
danielkjellid <daniel.kjellid@oda.com>
2022-04-13 12:21:03 +0200

    Add pytest

--- aria/settings.py
+++ aria/settings.py
@@ -138,6 +138,7 @@ DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
+    "django.contrib.sites",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
