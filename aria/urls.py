10bbd8010d54072d3ff1d6f8e5744d437d20a538
danielkjellid <daniel.kjellid@oda.com>
2022-04-13 12:55:18 +0200

    Add urls from v2

--- aria/urls.py
+++ aria/urls.py
@@ -3,17 +3,22 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+api_patterns = [
+    path("auth/", include("aria.auth.urls")),
+    path("products/", include("aria.products.urls")),
+    path("users/", include("aria.users.urls")),
+    path("categories/", include("aria.categories.urls")),
+]
+
 urlpatterns = [
     path("alpha/", admin.site.urls),
-    path("api/auth/", include("aria.core.urls")),
-    path("api/", include("aria.users.urls")),
     path("api/", include("aria.kitchens.urls")),
-    path("api/", include("aria.products.urls")),
-    path("api/", include("aria.product_categorization.urls")),
     path("api/", include("aria.notes.urls")),
     path("api/suppliers/", include("aria.suppliers.urls")),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/", include(api_patterns)),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static("/media/", document_root=settings.MEDIA_ROOT)
