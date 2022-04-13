a2587043453e1a4690fc4da8004739aa964478bc
danielkjellid <daniel.kjellid@oda.com>
2022-04-13 13:09:57 +0200

    Revert "Add users from v2"
    This reverts commit c3b230530ed581196dfe307637e59615c6e756ce.

--- aria/users/selectors.py
+++ aria/users/selectors.py
@@ -1,13 +1,9 @@
-from typing import List, Union
-
 from django.contrib.auth.models import Permission
-from django.db.models import QuerySet
 
-from aria.users.filters import UserFilter
 from aria.users.models import User
 
 
-def get_user_permissions(user: User) -> List[str]:
+def get_user_permissions(user: User) -> list:
     """
     Returns a list of a specific users permission names.
     """
@@ -22,7 +18,7 @@ def get_user_permissions(user: User) -> List[str]:
     return permissions
 
 
-def get_user_group_permissions(user: User) -> List[str]:
+def get_user_group_permissions(user: User) -> list:
     """
     Return a list of a users' group permission names.
     """
@@ -35,15 +31,3 @@ def get_user_group_permissions(user: User) -> List[str]:
     )
 
     return group_permissions
-
-
-def user_list(*, filters=None) -> Union[QuerySet, User]:
-    """
-    Returns a queryset of users based on given filters.
-    """
-
-    filters = filters or {}
-
-    qs = User.objects.all()
-
-    return UserFilter(filters, qs).qs
