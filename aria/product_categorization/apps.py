c76a4ef7d8dfe0e8d444d8c38e302f50c444ee23
danielkjellid <daniel.kjellid@oda.com>
2022-05-07 18:32:28 +0200

    Delete files and refs

--- aria/product_categorization/apps.py
+++ aria/product_categorization/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 
 class ProductCategorizationConfig(AppConfig):
     name = "aria.product_categorization"
-
-    def ready(self) -> None:
-        import aria.product_categorization.signals  # noqa: F401
