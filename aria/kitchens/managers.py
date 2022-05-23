8f0681704facaa86677ecead2be8dad6b7bc08de
danielkjellid <daniel.kjellid@oda.com>
2022-05-24 00:06:42 +0200

    Add docstring

--- aria/kitchens/managers.py
+++ aria/kitchens/managers.py
@@ -4,5 +4,7 @@ from aria.products.enums import ProductStatus
 
 class KitchenQuerySet(BaseQuerySet):
     def available(self):
-
+        """
+        Get kitchens with ProductStats Available.
+        """
         return self.filter(status=ProductStatus.AVAILABLE)
