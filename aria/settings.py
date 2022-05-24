7097f2d47d2a6289e9c3dbce5920dac8a3652190
danielkjellid <daniel.kjellid@oda.com>
2022-05-24 23:36:02 +0200

    Remove last traces of rest_framework

--- aria/settings.py
+++ aria/settings.py
@@ -314,31 +314,15 @@ LOGGING = {
 ##########
 
 
-##############
-# Simple JWT #
-##############
+################
+# API Auth JWT #
+################
 
-# New JWT code:
 JWT_ISSUER = "api.flis.no"
 JWT_SIGNING_KEY = SECRET_KEY
 JWT_ALGORITHM = "HS256"
 JWT_ACCESS_TOKEN_LIFETIME = timedelta(hours=1)
 JWT_REFRESH_TOKEN_LIFETIME = timedelta(days=30)
-
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
-    "ALGORITHM": "HS256",
-    "SIGNING_KEY": SECRET_KEY,
-    "VERIFYING_KEY": None,
-    "AUTH_HEADER_TYPES": ("JWT",),
-    "USER_ID_FIELD": "id",
-    "USER_ID_CLAIM": "user_id",
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
-    "TOKEN_TYPE_CLAIM": "token_type",
-}
 
 #########
 # Email #
@@ -351,23 +335,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL", default="")
-
-##################
-# REST framework #
-##################
-
-REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": env.list(
-        "DEFAULT_AUTHENTICATION_CLASSES",
-        default=["rest_framework_simplejwt.authentication.JWTAuthentication"],
-    ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
-    "DEFAULT_RENDERER_CLASSES": env.list(
-        "DEFAULT_RENDERER_CLASSES", default=["rest_framework.renderers.JSONRenderer"]
-    ),
-    "DATETIME_FORMAT": "%d. %B %Y %H:%M",
-    "DATETIME_INPUT_FORMATS": ["%d. %B %Y %H:%M"],
-}
 
 ##########
 # Sentry #
