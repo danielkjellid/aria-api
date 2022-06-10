4d65dbca323581be49d4c094e1549b71f9a00943
Daniel Kjellid <42611331+danielkjellid@users.noreply.github.com>
2022-06-10 19:14:19 +0200

    Add cached decorator, cache product list, update IaC (#143)
    * Add cached decorator, cache product list, update IaC
    
    * Add redis to CI

--- aria/core/decorators.py
+++ aria/core/decorators.py
@@ -1,7 +1,15 @@
 import functools
+import inspect
+import logging
+from typing import Any, Callable, Optional, TypeVar, cast
 
 from django.conf import settings
-from django.core.exceptions import PermissionDenied
+from django.core.cache import cache
+
+from aria.core.cache_utils import get_codec
+
+F = TypeVar("F", bound=Callable[..., Any])
+logger = logging.getLogger(__name__)
 
 
 class NotAllowedInProductionException(Exception):
@@ -25,49 +33,87 @@ def not_in_production(func):
     return inner
 
 
-def permission_required(permissions: str | list[str] | set[str], *, all_required=True):
+def cached(
+    *, key: str | Callable[..., str], timeout: Optional[int] = 60
+) -> Callable[[F], F]:
     """
-    Decorator that can be used alongside the any function to check if a user
-    has a particular permission. Raises a PermissionDenied exception if user
-    does not have the approariate permission.
+    Caches the result of a method using a key, being a string or function,
+    for an amount of seconds.
+
+    E.g:      @cached(key="my-key", timeout=(60 * 60))
+    or:       @cached(key=lambda instance: f"my-key.{instance.id}", timeout=(60 * 60))
+
+    NOTE: The lambda/key function must accept exactly the same arguments (*args and **kwargs)
+    as the decorated function with same arguments are passed to the key function.
+
+    This supports safely caching dataclasses, but only if the return value of the
+    decorated function is properly typed. Eg.:
+
+        @cached(key=lambda arg: f"my-key.{arg}")
+        def my_function(*, arg: str) -> MyDataclass:
+            ...
+
+    This also provides a helper to clear the cache. This must be provided with
+    the same arguments as the function.
+
+    E.g:    my_function.uncache(arg="hello")
     """
 
-    if not isinstance(permissions, (list, set)):
-        permissions = [permissions]
+    get_cache_key: Callable[..., str]
 
-    def decorator(func):
+    if isinstance(key, str) and key:
+        get_cache_key = lambda *args, **kwargs: cast(str, key)  # noqa
+    elif callable(key):
+        get_cache_key = key
+    else:
+        raise TypeError(f"Key must be a non-empty string or callable: {key}")
+
+    def uncache(*args: Any, **kwargs: Any) -> Any:
+        """
+        Helper to clear the cache. Takes the same arguments as the function.
+        """
+
+        cache_key = get_cache_key(*args, **kwargs)
+        cache.delete(cache_key)
+
+    def decorator(func: F) -> F:
+        signature = inspect.signature(func)
+        encoder, decoder = get_codec(type_annotation=signature.return_annotation)
+
         @functools.wraps(func)
-        def inner(*args, **kwargs):
-            *_, info = args
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            """
+            Returns the new function, wrapping fn and only calling it if no
+            value was found in the cache.
+            """
+
+            cache_key = get_cache_key(*args, **kwargs)
 
             try:
-                user = info.auth
+                cached_value = cache.get(cache_key)
+            except Exception as exc:
+                logger.error(f"Cache read failed for key {cache_key}", exc_info=exc)
+                cached_value = None
 
-                if not user:
-                    raise PermissionDenied("User from context is unknown.")
+            if cached_value is not None:
+                value = decoder(cached_value)
+            else:
+                # Value is not in cache, so calculate and update cache
+                value = func(*args, **kwargs)
+                value_for_cache = encoder(value)
 
-                if not user.is_authenticated:
-                    raise PermissionDenied("User is unauthenticated.")
+                try:
+                    cache.set(cache_key, value_for_cache, timeout=timeout)
+                except Exception as exc:
+                    logger.error(
+                        f"Cache write failed for key: {cache_key}", exc_info=exc
+                    )
 
-                if all_required:
-                    if not user.has_perms(permissions):
-                        raise PermissionDenied(
-                            "User does not have all the required permissions."
-                        )
-                else:
-                    if not user.has_perm(permissions):
-                        raise PermissionDenied(
-                            "User does not have any of the required permissions."
-                        )
+            return value
 
-                return func(*args, **kwargs)
-            except AttributeError:
-                raise AttributeError(
-                    "Auth attribute on WSGIRequest does not exist. "
-                    "Ensure that authentication for the endpoint is enabled "
-                    "before using the permission_required decorator"
-                )
+        # Add the uncache helper as a attribute of the function
+        inner.uncache = uncache
 
-        return inner
+        return cast(F, inner)
 
     return decorator
--- aria/core/tests/test_decorators.py
+++ aria/core/tests/test_decorators.py
@@ -1,6 +1,14 @@
+from dataclasses import dataclass
+
+from django.core.cache import cache
+
 import pytest
 
-from aria.core.decorators import NotAllowedInProductionException, not_in_production
+from aria.core.decorators import (
+    NotAllowedInProductionException,
+    cached,
+    not_in_production,
+)
 
 
 class TestCoreDecorators:
@@ -38,3 +46,37 @@ class TestCoreDecorators:
 
         with pytest.raises(NotAllowedInProductionException):
             func_a()
+
+    def test_cached_decorator(self):
+        @dataclass(frozen=True, eq=True)
+        class MyDataClass:
+            id: int
+            a_list: list[int]
+
+        @cached(key="func_a")
+        def func_a() -> MyDataClass:
+            func_a.num_times_called += 1
+            return MyDataClass(id=func_a.num_times_called, a_list=[1, 2])
+
+        @cached(key="func_b")
+        def func_b() -> list[MyDataClass]:
+            func_b.num_times_called += 1
+            return [MyDataClass(id=func_a.num_times_called, a_list=[1, 2])]
+
+        func_a.num_times_called = 0
+        func_b.num_times_called = 0
+
+        assert "func_a" not in cache
+        assert func_a() == MyDataClass(id=1, a_list=[1, 2])
+        assert cache.get("func_a") == {"id": 1, "a_list": [1, 2]}
+        assert func_a() == MyDataClass(id=1, a_list=[1, 2])
+        assert func_a.num_times_called == 1
+
+        assert "func_b" not in cache
+        assert func_b() == [MyDataClass(id=1, a_list=[1, 2])]
+        assert cache.get("func_b") == [{"id": 1, "a_list": [1, 2]}]
+        assert func_b() == [MyDataClass(id=1, a_list=[1, 2])]
+        assert func_a.num_times_called == 1
+
+        func_b.uncache()
+        assert "func-b" not in cache
