a5fa6500b8d2d8a870e3b6166a69920a1c8aa75c
danielkjellid <daniel.kjellid@oda.com>
2022-07-27 13:06:40 +0200

    Change bin decorators

--- bin/utils/decorators.py
+++ bin/utils/decorators.py
@@ -4,10 +4,10 @@ import sys
 
 def not_in_production(func):
     environment = os.environ.get("ENVIRONMENT")
-    is_production = os.environ.get("PRODUCTION")
+    is_production = os.environ.get("PRODUCTION", True)
 
     def inner(*args, **kwargs):
-        if environment == "production" or is_production:
+        if environment == "production" or is_production is True:
             sys.exit("This command can not be run in production!")
 
         return func(*args, **kwargs)
@@ -17,10 +17,10 @@ def not_in_production(func):
 
 def not_in_dev(func):
     environment = os.environ.get("ENVIRONMENT")
-    is_production = os.environ.get("PRODUCTION")
+    is_production = os.environ.get("PRODUCTION", False)
 
     def inner(*args, **kwargs):
-        if environment != "production" or not is_production:
+        if environment != "production" or is_production is False:
             sys.exit("This command is a production script, and can not be run locally!")
 
         return func(*args, **kwargs)
