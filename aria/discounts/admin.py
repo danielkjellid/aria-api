3332d84658a5a917dedec9e984e4be8f0f57fb71
danielkjellid <daniel.kjellid@oda.com>
2022-08-02 16:10:32 +0200

    Add admin

--- aria/discounts/admin.py
+++ aria/discounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from aria.discounts.models import ProductDiscount
+
+
+@admin.register(ProductDiscount)
+class ProductDiscountAdmin(admin.ModelAdmin):
+    model = ProductDiscount
+    list_display = (
+        "name",
+        "description",
+        "slug",
+    )
+    filter_horizontal = ("products",)
