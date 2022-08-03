12bfe8ab259f7aebfc7f1b3e8c0cb9b5719554c9
danielkjellid <daniel.kjellid@oda.com>
2022-08-03 15:01:54 +0200

    Fix url reverse in test and type

--- aria/products/selectors.py
+++ aria/products/selectors.py
@@ -5,10 +5,10 @@ from django.db.models import Prefetch, Q
 from aria.categories.models import Category
 from aria.categories.selectors import category_tree_active_list_for_product
 from aria.core.decorators import cached
-from aria.core.models import BaseQuerySet
 from aria.core.selectors import base_header_image_record
 from aria.products.enums import ProductStatus, ProductUnit
 from aria.products.filters import ProductSearchFilter
+from aria.products.managers import ProductQuerySet
 from aria.products.models import Product, ProductOption
 from aria.products.records import (
     ProductColorRecord,
@@ -173,7 +173,7 @@ def product_detail(
 
 def product_list_for_qs(
     *,
-    products: BaseQuerySet["Product"],
+    products: ProductQuerySet,
     filters: ProductListFilters | dict[str, Any] | None,
 ) -> list[ProductListRecord]:
     """
--- aria/products/tests/test_urls.py
+++ aria/products/tests/test_urls.py
@@ -6,7 +6,9 @@ class TestPublicProductsUrls:
         """
         Test reverse match of product_list_by_category_api endpoint.
         """
-        url = reverse("api-1.0.0:products-{category_slug}", args=["category_slug"])
+        url = reverse(
+            "api-1.0.0:products-category-{category_slug}", args=["category_slug"]
+        )
         assert url == "/api/products/category/category_slug/"
 
     def test_url_product_detail_api(self) -> None:
