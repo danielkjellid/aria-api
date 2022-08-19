fda179a8b2fd08480e138c2f96534c0ef4e1cfdf
danielkjellid <daniel.kjellid@oda.com>
2022-08-19 14:53:32 +0200

    Amend supplier squashed migration

--- aria/suppliers/migrations/0001_squashed_0005_alter_supplier_managers_remove_supplier_sites.py
+++ aria/suppliers/migrations/0001_squashed_0005_alter_supplier_managers_remove_supplier_sites.py
@@ -2,7 +2,6 @@
 
 import datetime
 
-import django.contrib.sites.managers
 import django.db.models.manager
 from django.db import migrations, models
 from django.utils.timezone import utc
@@ -32,7 +31,6 @@ class Migration(migrations.Migration):
                 ('supplier_discount', models.FloatField(blank=True, help_text='Supplier discount in percent. E.g. 0.2 = 20%', null=True)),
                 ('origin_country', django_countries.fields.CountryField(max_length=2)),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether the category should be treated as active.', verbose_name='Active')),
-                ('sites', models.ManyToManyField(blank=True, related_name='suppliers', to='sites.site')),
                 ('created_at', models.DateTimeField(auto_now_add=True, default=datetime.datetime(2022, 2, 25, 21, 13, 19, 459286, tzinfo=utc), verbose_name='created time')),
                 ('image', models.ImageField(blank=True, upload_to=aria.core.utils.get_static_asset_upload_path, verbose_name='Image')),
                 ('updated_at', models.DateTimeField(auto_now=True, verbose_name='modified time')),
