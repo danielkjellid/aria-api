10f9cab56cffcc01ac42601cc48726594ff1103b
danielkjellid <daniel.kjellid@oda.com>
2022-08-26 14:31:06 +0200

    Remove schema suffix

--- aria/front/endpoints/public.py
+++ aria/front/endpoints/public.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest
 from ninja import Router
 
 from aria.api.decorators import api
-from aria.front.schemas.outputs import OpeningHoursOutputSchema, SiteMessageOutputSchema
+from aria.front.schemas.outputs import OpeningHoursOutput, SiteMessageOutput
 from aria.front.selectors import (
     opening_hours_detail_from_cache,
     site_message_active_list_from_cache,
@@ -16,37 +16,35 @@ router = Router(tags=["Front"])
     router,
     "opening-hours/",
     method="GET",
-    response={200: OpeningHoursOutputSchema},
+    response={200: OpeningHoursOutput},
     summary="Get opening hours for a site",
 )
 def opening_hours_detail_api(
     request: HttpRequest,
-) -> tuple[int, OpeningHoursOutputSchema]:
+) -> tuple[int, OpeningHoursOutput]:
     """
     Retrieve opening hours for a single site instance based on site id.
     """
 
     opening_hours = opening_hours_detail_from_cache()
 
-    return 200, OpeningHoursOutputSchema(**opening_hours.dict())
+    return 200, OpeningHoursOutput(**opening_hours.dict())
 
 
 @api(
     router,
     "site-messages/active/",
     method="GET",
-    response={200: list[SiteMessageOutputSchema]},
+    response={200: list[SiteMessageOutput]},
     summary="Get site messages for a site",
 )
 def site_messages_active_list_api(
     request: HttpRequest,
-) -> list[SiteMessageOutputSchema]:
+) -> list[SiteMessageOutput]:
     """
     Retrieve a list of active site messages for a specific site.
     """
 
     site_messages = site_message_active_list_from_cache()
 
-    return [
-        SiteMessageOutputSchema(**site_message.dict()) for site_message in site_messages
-    ]
+    return [SiteMessageOutput(**site_message.dict()) for site_message in site_messages]
