9d5d18ae1b4513d360e0736e5ea237d96ee66e42
Daniel Kjellid <daniel.kjellid@gmail.com>
2022-09-13 23:03:57 +0200

    Convert back to using a single api with nested routers

--- aria/employees/tests/test_urls.py
+++ aria/employees/tests/test_urls.py
@@ -6,6 +6,6 @@ class TestPublicEmployeesUrls:
         """
         Test reverse match of employee_list_api endpoint.
         """
-        url = reverse("api-public-1.0.0:employees-index")
+        url = reverse("api-1.0.0:employees-index")
 
         assert url == "/api/v1/employees/"
