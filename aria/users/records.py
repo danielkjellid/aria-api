04c5b12cf2ab5f68f86f1760b776042bd328a03f
Daniel Kjellid <daniel.kjellid@gmail.com>
2022-12-24 14:24:12 +0100

    Refactor user_record selector

--- aria/users/records.py
+++ aria/users/records.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+
 from datetime import date, datetime
+from typing import TYPE_CHECKING, TypeVar
 
 from pydantic import BaseModel
+
+if TYPE_CHECKING:
+    from aria.users.models import User
 
 
 class UserProfileRecord(BaseModel):
@@ -31,6 +37,42 @@ class UserRecord(BaseModel):
     is_staff: bool = False
     is_superuser: bool = False
     permissions: list[str]
+
+    @classmethod
+    def from_user(
+        cls, user: "User", permissions: list[str] | None = None
+    ) -> "UserRecord":
+        """
+        Generate user record from model.
+        """
+
+        return cls(
+            id=user.id,
+            email=user.email,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            profile=UserProfileRecord(
+                full_name=user.full_name,
+                avatar_color=user.avatar_color,
+                initial=user.initial,
+            ),
+            birth_date=user.birth_date,
+            phone_number=user.phone_number,
+            has_confirmed_email=user.has_confirmed_email,
+            street_address=user.street_address,
+            zip_code=user.zip_code,
+            zip_place=user.zip_place,
+            disabled_emails=user.disabled_emails,
+            subscribed_to_newsletter=user.subscribed_to_newsletter,
+            allow_personalization=user.allow_personalization,
+            allow_third_party_personalization=user.allow_third_party_personalization,
+            acquisition_source=user.acquisition_source,
+            date_joined=user.date_joined,
+            is_active=user.is_active,
+            is_staff=user.is_staff,
+            is_superuser=user.is_superuser,
+            permissions=permissions if permissions else [],
+        )
 
 
 class UserNotesRecord(BaseModel):
